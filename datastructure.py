7f32d8482bc1f806bc22abcb8970595b671ddc2b
nathaniel <109425755+Dekpnn@users.noreply.github.com>
2026-05-25 02:07:19 +0100

    data structure in py

---
+++ datastructure.py
@@ -0,0 +1,13 @@
+# this is a list in python, can be manipulated 
+names = ["Avi", "dan", "daniel", "Grace", "Harry"]
+print(names[0])
+
+#This is a set type in python
+sset = set()
+
+sset.add(0)
+sset.add(1)
+sset.add(2)
+sset.add(3)
+print(sset)
+print(f"the length os this set is: {len(sset)}")
