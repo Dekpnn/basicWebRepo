9722e2759b8bd08536ccdba0e3f3e34b8b71c24d
nathaniel <109425755+Dekpnn@users.noreply.github.com>
2026-05-26 00:57:17 +0100

    Python mods

--- functions.py
+++ functions.py
@@ -3,5 +3,3 @@
 def square(x):
     return x*x
 
-for i in range(50):
-    print(f"The square of '{i}' is {square(i)}")
---
+++ squares.py
@@ -0,0 +1,5 @@
+from functions import square
+# or "import functions" then "functions.square(i)" to run the method
+
+for i in range(50):
+    print(f"The square of '{i}' is {square(i)}")
