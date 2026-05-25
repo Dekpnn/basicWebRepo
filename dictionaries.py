20ba89899f2bd2e1d90857ea7944475973b3eeef
nathaniel <109425755+Dekpnn@users.noreply.github.com>
2026-05-25 02:39:06 +0100

    modifications

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#Lets say we are mapping paople to where they live
+
+houses = {"Nathaniel": "Otuaba", "David": "PA", "Uche": "NDDC"}
+print(houses["Uche"])
+
+#Lets add a key to our map
+houses["Godswill"] = {"School gate"}
+
+print(houses["Godswill"])
