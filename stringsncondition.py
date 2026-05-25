7a87826964625eca709f3a8d6a94c6d9b8bab5b9
nathaniel <109425755+Dekpnn@users.noreply.github.com>
2026-05-25 02:06:40 +0100

    string and conditions

---
+++ stringsncondition.py
@@ -0,0 +1,16 @@
+#string
+print("Hello, World!")
+#formatted strings
+name = input("Name: ")
+print(f"Hello {name}! How are you?")
+
+#conditons
+
+n = int(input("number: "))
+
+if n>0:
+    print("N is positive")
+elif n<0:
+    print("N is negative")
+else:
+    print("N is zero")
